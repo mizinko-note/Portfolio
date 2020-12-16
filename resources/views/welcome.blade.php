cd88013bc920dc517d8abc2333351493bead6b45
mizinko_note <58818428+mizinko-note@users.noreply.github.com>
2020-12-16 17:55:40 +0900

    情報更新

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,13 +19,13 @@
 					   		</div>
 				   		</div>
 				   	</li>
-				   	<li style="background-image: url(images/img_bg_2.jpg);">
+				   	<li style="background-image: url(images/img_bg_5.jpg);">
 				   		<div class="overlay"></div>
 				   		<div class="container-fluid">
 				   			<div class="row">
 					   			<div class="col-md-8 col-md-offset-2 text-center js-fullheight slider-text">
 					   				<div class="slider-text-inner">
-					   					<h1>HTMLテンプレートを利用しています</h1>
+					   					<h1>HTMLテンプレートを利用しています。</h1>
 											<h2>Free html5 templates Made by <a href="http://freehtml5.co/" target="_blank">freehtml5.co</a></h2>
 											<p><a class="btn btn-primary btn-demo popup-vimeo" href="https://vimeo.com/channels/staffpicks/93951774"> <i class="icon-monitor"></i> Live Preview</a> <a class="btn btn-primary btn-learn">Learn More<i class="icon-arrow-right3"></i></a></p>
 					   				</div>
@@ -48,8 +48,8 @@
 							</div>
 							<div class="fh5co-text">
 								<h3>Linux</h3>
-                                <p>Amazon Linux 2（amzn2-ami-hvm-2.0.20201126.0-x86_64-gp2）を利用中<br>
-                                Dockerの中でApache2 (Debian)をWebサーバとして起動中。</p>
+        <p>Amazon Linux 2（amzn2-ami-hvm-2.0.20201126.0-x86_64-gp2）を利用中<br>
+        Dockerの中でApache2 (Debian)をWebサーバとして起動中。</p>
 							</div>
 						</div>
 					</div>
@@ -97,7 +97,7 @@
 							</div>
 							<div class="fh5co-text">
 								<h3>Icons</h3>
-								<p>比較的自由にカスタマイズ可能なIcoMoonを利用中</p>
+								<p>自由にカスタマイズ可能なIcoMoonを利用中</p>
 							</div>
 						</div>
 					</div>
@@ -119,14 +119,14 @@
 			<div class="fh5co-narrow-content">
 				<h2 class="fh5co-heading animate-box" data-animate-effect="fadeInLeft">Contact</h2>
 				<div class="row">
-					<div class="col-md-6">
+				 <div class="col-md-6">
 						<div class="fh5co-feature animate-box" data-animate-effect="fadeInLeft">
 							<div class="fh5co-icon">
-								<i class="icon-twitter2"></i>
+								<i class="icon-github"></i>
 							</div>
 							<div class="fh5co-text">
-								<h3>Twitter</h3>
-								<p><a href="https://twitter.com/mizinko_note" target="_blank" >@mizinko_note</a>までご連絡ください。</p>
+								<h3>Github</h3>
+								<p><a href="https://github.com/mizinko-note/" target="_blank" >mizinko_note</a></p>
 							</div>
 						</div>
 					</div>
@@ -143,17 +143,6 @@
 					</div>
 				</div>
 				<div class="row">
-					<div class="col-md-6">
-						<div class="fh5co-feature animate-box" data-animate-effect="fadeInLeft">
-							<div class="fh5co-icon">
-								<i class="icon-github"></i>
-							</div>
-							<div class="fh5co-text">
-								<h3>Github</h3>
-								<p><a href="https://github.com/mizinko-note/" target="_blank" >mizinko_note</a></p>
-							</div>
-						</div>
-					</div>
 				</div>
 			</div>
 		</div>
